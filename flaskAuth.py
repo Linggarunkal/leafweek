e17852b066699e0021d06200dc69b083f4b76bbb
Linggarunkal <linggar@outlook.com>
2018-02-20 17:44:37 +0700

    add new configure

--- flaskAuth.py
+++ flaskAuth.py
@@ -38,9 +38,9 @@ class User(db.Model):
         try:
             data = s.loads(token)
         except SignatureExpired:
-            return jsonify({'Error': '500', 'Message': 'Token Expired'})
+            return None
         except BadSignature:
-            return jsonify({'Error': '400', 'Message': 'Invalid Token'})
+            return None
         user = User.query.get(data['id'])
         return user
 
@@ -63,6 +63,7 @@ def new_user():
     username = request.json.get('username')
     password = request.json.get('password')
     if username is None or password is None:
+
         abort(400)    # missing arguments
     if User.query.filter_by(username=username).first() is not None:
         abort(400)    # existing user
